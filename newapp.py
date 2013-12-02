3e044fcd308addf17443f3c3147e2647e30dd4ea
Michael Luckeneder <michael@luckeneder.net>
2013-12-01 22:24:14 -0800

    added flask app

--- newapp.py
+++ newapp.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask.ext.mako import MakoTemplates
+from flask.ext.mako import render_template
+
+from coarticles import create_manager as manager
 
 app = Flask(__name__)
+mako = MakoTemplates(app)
+
 
 @app.route('/')
 def index():
-    return "<span style='color:red'>I am app 1</span>"
+    arts = manager().get_all_articles()
+    return render_template('index.html', articles=arts.items())
+
+if __name__ == '__main__':
+    app.run(debug=True)
